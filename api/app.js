5452a3798c54ee84b979a04e7b9af327229452ec
Praveen-Bharath <91447499+Praveen-Bharath@users.noreply.github.com>
2021-11-17 18:23:39 +0530

    Update app.js

--- api/app.js
+++ api/app.js
@@ -15,9 +15,14 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+let corsOptions = {
+  origin: 'trustedwebsite.com' // Compliant
+};
+
+let app = express();
+app.use(cors(corsOptions));
 app.use(logger('dev'));
 app.use(express.json());
-app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
